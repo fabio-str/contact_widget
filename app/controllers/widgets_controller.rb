65fdb13052b4589b35d2977a66a8f411d833add9
fabio-str <fabio93straessle@hotmail.com>
2023-06-30 09:55:16 +0200

    improved form frontend

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -1,13 +1,9 @@
 class WidgetsController < ApplicationController
-  before_action :set_widget, only: %i[ show edit update destroy ]
+  before_action :set_widget, only: %i[ edit update destroy ]
 
   # GET /widgets or /widgets.json
   def index
     @widgets = Widget.all
-  end
-
-  # GET /widgets/1 or /widgets/1.json
-  def show
   end
 
   # GET /widgets/new
@@ -21,11 +17,11 @@ class WidgetsController < ApplicationController
 
   # POST /widgets or /widgets.json
   def create
-    @widget = Widget.new(widget_params)
+    @widget = current_user.widgets.new(widget_params)
 
     respond_to do |format|
       if @widget.save
-        format.html { redirect_to widget_url(@widget), notice: "Widget was successfully created." }
+        format.html { redirect_to widgets_url, notice: "Widget was successfully created." }
         format.json { render :show, status: :created, location: @widget }
       else
         format.html { render :new, status: :unprocessable_entity }
